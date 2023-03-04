c566bc6a4e5ecee3442a677078eaf2d283734841
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-03-04 14:48:46 +0100

    delete codes

--- client/src/Components/Home/Landing.js
+++ client/src/Components/Home/Landing.js
@@ -3,63 +3,19 @@ import { useInView } from "react-intersection-observer";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-// import Navbar from '../General/Navbar';
-import landingVideo from '../Media/videos/landing.mp4';
-// import coverPhoto from '../Media/images/cover_1.png';
+// import landingVideo from '../Media/videos/landing.mp4';
 
 const Landing = () => {
-    const { ref: shopRef, inView: shopVisible } = useInView();
-    const { ref: landingRef, inView: compressLanding } = useInView();
+    // const { ref: shopRef, inView: shopVisible } = useInView();
+    // const { ref: landingRef, inView: compressLanding } = useInView();
 
     useEffect(() => {
         AOS.init();
     });
 
     return (
-        <div className={`landing ${compressLanding} ? animate-landing : ''`} ref={landingRef} >
-            <div className="l-video">
-                <video className={`landing-video ${compressLanding} ? animate-video : ''`} autoPlay muted>
-                    <source src={landingVideo} type="video/mp4"></source>
-                </video>
-                <div className={`landing-cover ${compressLanding} ? animate-video : ''`} ref={shopRef}>
-                    <div className="celine">
-                        <h1
-                            data-aos="fade-right"
-                            data-aos-duration="2000"
-                        >
-                            C
-                        </h1>
-                        <h1>
-                            É
-                        </h1>
-                        <h1
-                            data-aos="fade-left"
-                            data-aos-duration="2000"
-                        >
-                            L
-                        </h1>
-                        <h1
-                            data-aos="fade-left"
-                            data-aos-duration="2000"
-                        >
-                            I
-                        </h1>
-                        <h1
-                            data-aos="fade-left"
-                            data-aos-duration="2000"
-                        >
-                            N
-                        </h1>
-                        <h1
-                            data-aos="fade-left"
-                            data-aos-duration="2000"
-                        >
-                            E
-                        </h1>
-                    </div>
-                    <div className={`circle icon ${shopVisible} ? animate-shop : ''`}><h5 className={`text ${shopVisible} ? animate-text : ''`}>SHOP NOW</h5></div>
-                </div>
-            </div>
+        <div>
+            
         </div>
     );
 };
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import Navbar from '../Components/General/Navbar';
 import Landing from '../Components/Home/Landing';
-import Main from '../Components/Home/Main';
-import Poster from '../Components/Home/Poster';
-import Maintext from '../Components/Home/Maintext';
-import Clothstyles from '../Components/Home/Clothstyles';
 
 
 const Home = () => {
@@ -12,10 +8,6 @@ const Home = () => {
         <div className='home'>
             <Navbar />
             <Landing />
-            <Main />
-            <Poster />
-            <Maintext />
-            <Clothstyles />
         </div>
     );
 };
