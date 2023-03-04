1fa62b4fc59e9cc87aa6371938637aae94a95529
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-03-04 17:06:05 +0100

    landing page

--- client/src/Components/Home/Landing.js
+++ client/src/Components/Home/Landing.js
@@ -14,8 +14,16 @@ const Landing = () => {
     });
 
     return (
-        <div>
-            
+        <div className='landing'>
+            <div className="top">
+                <h1>c<span>é</span>line</h1>
+            </div>
+            <div className="scroll-text">
+                <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam, cumque. Dolorum perferendis quae provident dolorem, qui aspernatur, facilis in praesentium omnis possimus tempore. Temporibus tenetur eius accusamus neque aliquam iure?</h1>
+            </div>
+            <div className="slider">
+                <h5>slider</h5>
+            </div>
         </div>
     );
 };
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import Navbar from '../Components/General/Navbar';
+
 import Landing from '../Components/Home/Landing';
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 const Home = () => {
     return (
         <div className='home'>
-            <Navbar />
+           
+            <div className="lines">
+                <div className="one"></div>
+                <div className="two"></div>
+                <div className="three"></div>
+                <div className="four"></div>
+                <div className="five"></div>
+                <div className="six"></div>
+            </div>
+            <div className="blocks">
+
+            </div>
             <Landing />
+
+            <h5 id='author'>CREATED BY DJ</h5>
+             <div className="menu">
+                <MenuIcon id='menu' />
+            </div>
         </div>
     );
 };
