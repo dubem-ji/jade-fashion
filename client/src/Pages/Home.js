6a966d9936a8bacb1e105cf58ee3a0d914c62cd9
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-03-08 18:39:09 +0100

    update landing page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -8,6 +8,56 @@ import Clothstyles from '../Components/Home/Clothstyles';
 
 
 const Home = () => {
+
+    const toggleBackground = useRef(null);
+    const triggerBrown = useRef(null);
+    const triggerGrey = useRef(null);
+    const showFooter = useRef(null)
+
+    useEffect(() => {
+        const backgroundColor = toggleBackground.current;
+        const brownTrigger = triggerBrown.current;
+        const greyTrigger = triggerGrey.current;
+            
+        gsap.to(backgroundColor, {
+            background: '#513233',
+            color: '#FFFFFF',
+            scrollTrigger: {
+                trigger: brownTrigger,
+                start: 'top 90%',
+                end: 'top 0%',
+                scrub: true,
+                toggleActions: 'restart pause reverse complete',
+                // markers: true,
+            }
+        },)
+
+        gsap.to(backgroundColor, {
+            background: '#202322',
+            color: '#FFFFFF',
+            scrollTrigger: {
+                trigger: greyTrigger,
+                start: 'top 59%',
+                end: 'top 0%',
+                scrub: true,
+                toggleActions: 'restart pause reverse complete',
+                // markers: true,
+            }
+        },)
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: greyTrigger,
+                start: 'top 20%',
+                end: 'top 10%',
+                scrub: 2,
+                markers: true,
+            }
+        })
+        tl.to('.footer', {height:'50vh', duration: 10})
+
+    }, [])
+
     return (
         <div className='home'>
             <Navbar />
