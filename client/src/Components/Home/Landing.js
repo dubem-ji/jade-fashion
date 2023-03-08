79540a2bab6e570a4c44ecdb314120412a9f6c93
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-03-08 18:04:56 +0100

    update home page

--- client/src/Components/Home/Landing.js
+++ client/src/Components/Home/Landing.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef } from 'react';
+// import { Link } from 'react-router-dom';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-// import landingVideo from '../Media/videos/landing.mp4';
 import photoOne from '../Media/images/photocard_1.jpg';
 import photoTwo from '../Media/images/photocard_2.jpg';
 import photoThree from '../Media/images/photocard_3.jpg';
@@ -16,6 +16,7 @@ gsap.registerPlugin(ScrollTrigger)
 
 const Landing = () => {
     const scrollText = useRef(null);
+    const scrollDescrip = useRef(null);
     const triggerScroll = useRef(null);
     const scrollPhone = useRef(null);
     const movePhotoOne = useRef(null);
@@ -24,10 +25,12 @@ const Landing = () => {
     const movePhotoFour = useRef(null);
     const movePhotoFive = useRef(null);
     const movePhotoSix = useRef(null);
+    const showScrolls = useRef(null);
     
 
     useEffect(() => {
         const el = scrollText.current;
+        const descrip = scrollDescrip.current;
         const phone = scrollPhone.current;
         const photoOne = movePhotoOne.current;
         const photoTwo = movePhotoTwo.current;
@@ -36,6 +39,7 @@ const Landing = () => {
         const photoFive = movePhotoFive.current;
         const photoSix = movePhotoSix.current;
         const triggerEl = triggerScroll.current;
+        const scrolls = showScrolls.current;
         
         gsap.to(triggerEl, {
             x: -2500,
@@ -52,8 +56,20 @@ const Landing = () => {
             }
         },)
 
+         gsap.to(descrip, {
+            opacity: 0,
+            scrollTrigger: {
+                trigger: el,
+                start: "top 20%",
+                end: () => `+=${el.offsetWidth/10 }`,
+                scrub: true,
+                toggleActions: 'restart pause reverse complete',
+            }
+        },)
+
         gsap.to(phone, {
             top: "0%",
+            height: '100%',
             scrollTrigger: {
                 trigger: el,
                 start: "top 10%",
@@ -62,13 +78,24 @@ const Landing = () => {
                 toggleActions: 'restart pause reverse complete',
             }
         },)
-
+        
+        gsap.to(scrolls, {
+            top: '0%',
+            opacity: '1',
+            scrollTrigger: {
+                trigger: el,
+                start: "top 10%",
+                end: () => `+=${el.offsetWidth / 4}`,
+                scrub: true,
+                toggleActions: 'restart pause reverse complete',
+            }
+        },)
         
         gsap.to(photoOne, {
             top: "20%",
             right: '39.8%',
             height: '300px',
-            width: '325px',
+            width: '328px',
             scrollTrigger: {
                 trigger: el,
                 start: "top 20%",
@@ -77,28 +104,27 @@ const Landing = () => {
                 toggleActions: 'restart pause reverse complete',
             }
         },)
-
          
         gsap.to(photoTwo, {
             top: "20%",
             right: '39.8%',
             height: '300px',
-            width: '325px',
+            width: '328px',
             scrollTrigger: {
                 trigger: el,
                 start: "top 20%",
                 end: () => `+=${el.offsetWidth / 4}`,
                 scrub: true,
                 toggleActions: 'restart pause reverse complete',
+                // markers: true,
             }
         },)
 
-         
         gsap.to(photoThree, {
             top: "20%",
             right: '39.8%',
             height: '300px',
-            width: '325px',
+            width: '328px',
             scrollTrigger: {
                 trigger: el,
                 start: "top 20%",
@@ -107,13 +133,12 @@ const Landing = () => {
                 toggleActions: 'restart pause reverse complete',
             }
         },)
-
          
         gsap.to(photoFour, {
             top: "20%",
             right: '39.8%',
             height: '300px',
-            width: '325px',
+            width: '328px',
             scrollTrigger: {
                 trigger: el,
                 start: "top 20%",
@@ -124,25 +149,10 @@ const Landing = () => {
         },)
 
         gsap.to(photoFive, {
-                top: "20%",
-                right: '39.8%',
-                height: '300px',
-                width: '325px',
-                scrollTrigger: {
-                    trigger: el,
-                    start: "top 20%",
-                    end: () => `+=${el.offsetWidth / 4}`,
-                    scrub: true,
-                    toggleActions: 'restart pause reverse complete',
-                }
-            },);
-
-         
-        gsap.to(photoSix, {
             top: "20%",
             right: '39.8%',
             height: '300px',
-            width: '325px',
+            width: '328px',
             scrollTrigger: {
                 trigger: el,
                 start: "top 20%",
@@ -151,28 +161,80 @@ const Landing = () => {
                 toggleActions: 'restart pause reverse complete',
             }
         },)
+         
+        gsap.to(photoSix, {
+            top: "20%",
+            right: '39.8%',
+            height: '300px',
+            width: '328px',
+            scrollTrigger: {
+                trigger: el,
+                start: "top 20%",
+                end: () => `+=${el.offsetWidth / 4}`,
+                scrub: true,
+                toggleActions: 'restart pause reverse complete',
+            }
+        },)
+
+        // #slides all photos left
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: el,
+                start: () => `+=${el.offsetWidth / 4 - 100}`,
+                end: () => `+=${el.offsetWidth / 4 + 100}`,
+                scrub: 2,
+                // markers: true,
+            }
+        })
+        tl.to('.photo', {x: -2000, duration: 5})
+
+
+
     }, [])
 
 
     return (
         <div className='landing'>
+
+            {/* #Text */}
             <div ref={triggerScroll} className="scroll-text">
                 <h1 ref={scrollText}>Lorem ipsum dolor, sit amet consectetur <span>adipisicing elit?</span></h1>
             </div>
-            <h3 className='descrip-text'>
+            <h3 ref={scrollDescrip} className='descrip-text'>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             </h3>
+
+            {/* #Phone
             <div className="show-phone">
                 <img className='mask' ref={scrollPhone} src={"https://upload.wikimedia.org/wikipedia/commons/3/32/IPhone_X_vector.svg"} alt="" />
             </div>
+            <div ref={showScrolls} className="scrolls">
+                <div className="empty">
 
-                <div ref={movePhotoFive} className="photo" id='five'><img src={photoFive} alt="" /></div>
-                <div ref={movePhotoSix} className="photo" id="six"><img  src={photoSix} alt="" /></div>
-                <div ref={movePhotoOne} className="photo" id='one'><img src={photoOne} alt="" /></div>
-                <div ref={movePhotoFour} className="photo" id='four'><img src={photoFour} alt="" /></div>
-                <div ref={movePhotoThree} className="photo" id='three'><img src={photoThree} alt="" /></div>
-                <div ref={movePhotoTwo} className="photo" id='two'><img src={photoTwo} alt="" /></div>
+                </div>
+                <div className="phone-background">
+                    <div className="b-color">
 
+                    </div>
+                </div>
+                <div className="empty">
+
+                </div>
+            </div> */}
+
+            {/* #Photos */}
+            <div ref={movePhotoFive} className="photo" id='five'><img src={photoFive} alt="" /></div>
+            <div ref={movePhotoSix} className="photo" id="six"><img src={photoSix} alt="" /></div>
+            <div ref={movePhotoOne} className="photo" id='one'><img src={photoOne} alt="" /></div>
+            <div ref={movePhotoFour} className="photo" id='four'><img src={photoFour} alt="" /></div>
+            <div ref={movePhotoThree} className="photo" id='three'><img src={photoThree} alt="" /></div>
+            <div ref={movePhotoTwo} className="photo" id='two'><img src={photoTwo} alt="" /></div>
+
+           
+
+            <div className="text-s1">
+
+            </div>
         </div>
     );
 };
