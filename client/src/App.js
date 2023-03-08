5e50b6551951f64148fc6216650d92e4f43aa4a5
Joy Iroegbu <101984538+Dubem-Joy@users.noreply.github.com>
2023-03-08 18:45:35 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
-import Shop from './Components/Shop/Shop';
+import Shop from './Components/Shop/Shop'
 
 function App() {
   return (
